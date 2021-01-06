86e8862d49867ae49ea1650b24b8cea3d6298c8f
moonjung <luminousmoonjj@gmail.com>
2021-01-06 17:11:10 +0900

    비밀 정보 보호

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema{
+const userSchema = mongoose.Schema ({
     name:{
         type:String, 
         maxlength:50
@@ -31,7 +31,8 @@ const userSchema = mongoose.Schema{
     tokenExp:{
         type: Number
     }
+})
 
+const User = mongoose.model('User',userSchema)
 
-
-}
+module.exports={User }
